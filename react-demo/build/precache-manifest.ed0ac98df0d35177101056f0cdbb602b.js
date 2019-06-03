b14caa01a633aea217791f42cede0cfe125ad7af
rainyjune <rainyjune@live.cn>
2019-06-03 15:34:16 +0800

    Add build directory

---
+++ react-demo/build/precache-manifest.ed0ac98df0d35177101056f0cdbb602b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645da13406bd1b435013bef60de6ec35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c520a6c75ead3879bee",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "60e50d85cfe1076882c0",
+    "url": "/static/js/2.d3f4b862.chunk.js"
+  },
+  {
+    "revision": "8c520a6c75ead3879bee",
+    "url": "/static/js/main.8ee6f519.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

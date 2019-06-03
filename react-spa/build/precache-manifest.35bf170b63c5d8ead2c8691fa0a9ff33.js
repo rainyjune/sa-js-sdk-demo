81b6f3c71f9cefecba293a9b22e69cf2d5dce9fd
rainyjune <rainyjune@live.cn>
2019-06-03 16:50:41 +0800

    add build files

---
+++ react-spa/build/precache-manifest.35bf170b63c5d8ead2c8691fa0a9ff33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b0f6df46bec3d4c727aa49a0df9bda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21ec4b71191da5ff4389",
+    "url": "/static/css/main.075c835f.chunk.css"
+  },
+  {
+    "revision": "305aa3c63f3879b85a49",
+    "url": "/static/js/2.6e925ae2.chunk.js"
+  },
+  {
+    "revision": "21ec4b71191da5ff4389",
+    "url": "/static/js/main.4e781763.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

2f5e22358075088f94d5d5a93abd57bd84bd562e
rainyjune <rainyjune@live.cn>
2019-06-05 12:43:12 +0800

    Upate

--- react/index.jsx
+++ react/index.jsx
@@ -3,4 +3,19 @@ import ReactDOM from "react-dom";
 import "reboot.css";
 import App from "./src/App";
 
+// 引入 SDK
+import sensors from'sa-sdk-javascript';
+
+// 初始化
+sensors.init({
+  //SDK版本1.12.18以上支持，默认值为false
+  is_track_single_page: true,
+  server_url: '//test-syg.datasink.sensorsdata.cn/sa?token=27f1e21b78daf376&project=lixiang',
+  heatmap: {}
+});
+
+//注意默认的： sensors.quick('autoTrack') 是需要的
+sensors.quick('autoTrack');
+
+
 ReactDOM.render(<App />, document.getElementById("app"));

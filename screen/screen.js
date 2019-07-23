d0b76935322fee1849b43a6e6bb441b7c2af5cd0
rainyjune <rainyjune@live.cn>
2019-07-23 13:00:07 +0800

    Update screen/screen.js

--- screen/screen.js
+++ screen/screen.js
@@ -1,9 +1,14 @@
 var elements = document.querySelectorAll('[id]');
 
-for (var i = elements.length - 1; i >= 0; i--) {
-  var element = elements[i];
-  element.innerHTML = getGlobalVarFromStr(element.id);
+function updateAllData() {
+  for (var i = elements.length - 1; i >= 0; i--) {
+    var element = elements[i];
+    element.innerHTML = getGlobalVarFromStr(element.id);
+  }
 }
+
+updateAllData();
+
 
 document.querySelector('[data-myid="screenWidth"]').innerHTML = window.screen.width * window.devicePixelRatio;
 document.querySelector('[data-myid="screenHeight"]').innerHTML = window.screen.height * window.devicePixelRatio;
@@ -16,3 +21,7 @@ function getGlobalVarFromStr(str) {
   }
   return currentVar;
 }
+
+window.addEventListener("scroll", function (event) {
+  updateAllData();
+});

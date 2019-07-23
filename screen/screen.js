1e2c76328c4093804d380c7294978e189c1cae7b
rainyjune <rainyjune@live.cn>
2019-07-23 13:46:09 +0800

    Update screen files

--- screen/screen.js
+++ screen/screen.js
@@ -1,17 +1,22 @@
-var elements = document.querySelectorAll('[id]');
+var elements = ["screen.width", "innerWidth", "document.body.clientWidth", "document.documentElement.clientWidth", "outerWidth", "document.documentElement.scrollWidth", "document.documentElement.offsetWidth", "document.body.scrollWidth", "document.body.offsetWidth", "screen.height", "innerHeight", "document.body.clientHeight", "document.documentElement.clientHeight", "outerHeight", "document.documentElement.scrollHeight", "document.documentElement.offsetHeight", "document.body.scrollHeight", "document.body.offsetHeight", "devicePixelRatio", "pageYOffset", "document.body.scrollTop", "document.documentElement.scrollTop"];
 
-function updateAllData() {
-  for (var i = elements.length - 1; i >= 0; i--) {
-    var element = elements[i];
-    element.innerHTML = getGlobalVarFromStr(element.id);
+var scrollArr = ["pageYOffset", "document.body.scrollTop", "document.documentElement.scrollTop"];
+
+function updateAllData(arr) {
+  arr = arr || elements;
+  for (var i = arr.length - 1; i >= 0; i--) {
+    var element = document.getElementById(arr[i]);
+    var val = getGlobalVarFromStr(element.id);
+    element.innerHTML = isNaN(val) ? 'undefined' : val;
   }
 }
 
 updateAllData();
 
+var myDevicePixelRatio = (isNaN(window.devicePixelRatio) ? 1 : window.devicePixelRatio);
 
-document.querySelector('[data-myid="screenWidth"]').innerHTML = window.screen.width * window.devicePixelRatio;
-document.querySelector('[data-myid="screenHeight"]').innerHTML = window.screen.height * window.devicePixelRatio;
+document.getElementById("screenWidth").innerHTML = window.screen.width * myDevicePixelRatio;
+document.getElementById("screenHeight").innerHTML = window.screen.height * myDevicePixelRatio;
 
 function getGlobalVarFromStr(str) {
   var currentVar = window;
@@ -22,9 +27,9 @@ function getGlobalVarFromStr(str) {
   return currentVar;
 }
 
-window.addEventListener("scroll", function (event) {
-  updateAllData();
-});
+window.onscroll = function (event) {
+  updateAllData(scrollArr);
+};
 
 
 window.onresize = function(event) {
